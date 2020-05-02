b8ceadbf402cda14104751d7ccf4ac1102249987
Sanskar Agarwal <47058176+Sanskar31@users.noreply.github.com>
2020-05-02 15:43:37 +0530

    Update LongestPathInADGraph.cpp
    Longest Path In An Acyclic Directed Graph

--- Dynamic Programming/LongestPathInADGraph.cpp
+++ Dynamic Programming/LongestPathInADGraph.cpp
@@ -67,9 +67,12 @@ void solve()
 int main()
 {
 #ifndef ONLINE_JUDGE
-	freopen("../input.txt", "r", stdin);
-	freopen("../output.txt", "w", stdout);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 #endif
+	/*
+	Problem Statement-- https://atcoder.jp/contests/dp/tasks/dp_g
+	*/
 	fast
 	ll t;
 	// cin >> t;
